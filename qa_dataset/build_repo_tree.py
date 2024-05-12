0e501e471c2649d6a4bbf641713f4762369dc3d0
Shuai Yan <yanshuairyan@gmail.com>
2024-05-11 22:10:27 -0700

    fix some file answer is empty

--- qa_dataset/build_repo_tree.py
+++ qa_dataset/build_repo_tree.py
@@ -1,11 +1,12 @@
 import sys
 import os
+
 project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_path)
 
-
 from qa_dataset.file_node import File, PythonFile
 from qa_dataset.folder_node import Folder
+
 
 def build_repo_tree(root_path):
     if os.path.isfile(root_path):
@@ -24,5 +25,7 @@ if __name__ == '__main__':
     from pathlib import Path
 
     CODE_ROOT = str(Path(__file__).parent.parent.absolute()) + os.sep + "nanoGPT"
-    res = build_repo_tree(CODE_ROOT)
-    print(res)
+    root = build_repo_tree(CODE_ROOT)
+    qa = root.prepare_qa()
+
+    print(qa)
--- qa_dataset/file_node.py
+++ qa_dataset/file_node.py
@@ -1,63 +1,22 @@
 import os.path
-
 from collections import defaultdict
+
 import libcst as cst
+
 from qa_dataset.qa_base_node import Node
 from qa_dataset.function_node import PyFunction
 from qa_dataset.class_node import PyClass
-
 from constant import PROJECT_ROOT
+
 
 def cst_parse_python_file(file_path):
     with open(file_path, 'r') as file:
         source_code = file.read()
-    
+
     # Parse the source code into a CST
     parsed_cst = cst.parse_module(source_code)
-    
+
     return parsed_cst
-
-
-
-class PythonFile(Node):
-    name_path_dict = defaultdict(set)
-
-    def __init__(self, path):
-        super().__init__()
-        self.class_name = "PythonFile"
-        self.name = os.path.basename(path)
-        self.path = path
-        self.content = None
-        self.import_from_file = []
-        self.cst_node = cst_parse_python_file(path)
-
-        path_to_project = os.path.relpath(path, PROJECT_ROOT)
-        PythonFile.name_path_dict[self.name].add(path_to_project)
-        self.qa_functions = [where_file_question]
-
-        self.top_level_code = self.get_top_level_code()
-        self.build_children_nodes()
-
-
-    def build_children_nodes(self):
-        for node in self.cst_node.body:
-            if isinstance(node, cst.FunctionDef):
-                self.children.append(PyFunction(node))
-            elif isinstance(node, cst.ClassDef):
-                self.children.append(PyClass(node))
-        return
-    
-    def get_top_level_code(self):
-        top_level_code = []
-        for node in self.cst_node.body:
-            # Checking for non-function and non-class top-level elements
-            if not isinstance(node, cst.FunctionDef) and not isinstance(node, cst.ClassDef):
-                # Using cst.Module to convert node back to source code string
-                top_level_code.append(cst.Module(body=[node]).code)
-
-        # Joining all the elements into a single string with newline characters
-        return "\n".join(top_level_code)
-
 
 
 class File(Node):
@@ -65,6 +24,8 @@ class File(Node):
 
     def __init__(self, path):
         super().__init__()
+        self.class_name = "File"
+
         self.name = os.path.basename(path)
         self.path = path
         self.content = None
@@ -79,10 +40,40 @@ class File(Node):
         return "\t" * level + f"File({self.name})\n"
 
 
+class PythonFile(File):
+
+    def __init__(self, path):
+        super().__init__(path)
+        self.class_name = "PythonFile"
+
+        self.cst_node = cst_parse_python_file(path)
+        self.qa_functions = self.qa_functions + []  # Add more functions here
+
+        self.top_level_code = self.get_top_level_code()
+        self.build_children_nodes()
+
+    def build_children_nodes(self):
+        for node in self.cst_node.body:
+            if isinstance(node, cst.FunctionDef):
+                self.children.append(PyFunction(node))
+            elif isinstance(node, cst.ClassDef):
+                self.children.append(PyClass(node))
+        return
+
+    def get_top_level_code(self):
+        top_level_code = []
+        for node in self.cst_node.body:
+            # Checking for non-function and non-class top-level elements
+            if not isinstance(node, cst.FunctionDef) and not isinstance(node, cst.ClassDef):
+                # Using cst.Module to convert node back to source code string
+                top_level_code.append(cst.Module(body=[node]).code)
+
+        # Joining all the elements into a single string with newline characters
+        return "\n".join(top_level_code)
+
+
 def where_file_question(file):
     question = f"where is the file {file.name} located?"
     prompt = None
     answer = "\n" + "\n".join(File.name_path_dict[file.name])
     return question, prompt, answer
-
-
